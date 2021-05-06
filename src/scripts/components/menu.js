c1e335d50405dd3cd647710523642e644c18ad2a
Ivan Zhubrov <izhubrov@rek-21.ru>
2021-05-06 13:19:35 +0300

    fix: menu-text

--- src/scripts/components/menu.js
+++ src/scripts/components/menu.js
@@ -2,7 +2,7 @@ export default class Menu {
   constructor(menuSettings) {
     this._handleClose = this._handleCloseMenu.bind(this);
     this._menuSettings = menuSettings;
-    this._mediaQuery = window.matchMedia("(min-width: 1280px)");
+    this._mediaQuery = window.matchMedia("(min-width: 1320px)");
   }
 
   _handleViewMenu() {
@@ -21,13 +21,10 @@ export default class Menu {
   _toggleClassList() {
     this._menuSettings.header.classList.toggle("header__overlay");
     this._menuSettings.header.classList.toggle("header_fixed");
-    // this._menuSettings.menu.classList.toggle('header__overlay');
     this._menuSettings.menu.classList.toggle("header__item_active");
     this._menuSettings.closeButton.classList.toggle("header__item_active");
     this._menuSettings.menuButton.classList.toggle("header__item_inactive");
-    this._menuSettings.personalAccountButton.classList.toggle(
-      "header__item_active"
-    );
+    this._menuSettings.personalAccountButton.classList.toggle("header__item_active");
     this._menuSettings.content.classList.toggle("content_inactive");
   }
 
