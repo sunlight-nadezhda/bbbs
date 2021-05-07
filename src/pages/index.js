b04282a11de8a8efbe6b0ff2a711f80e36b1ac5c
Ivan Zhubrov <izhubrov@rek-21.ru>
2021-05-07 14:19:31 +0300

    feat: registration popup

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,28 @@
-import './index.css';
-import Menu from '../scripts/components/Menu.js';
-import {menuSettings} from '../scripts/utils/utils.js';
+import "./index.css";
+import Menu from "../scripts/components/Menu.js";
+import { menuSettings, popupLogin } from "../scripts/utils/utils.js";
 
 const menu = new Menu(menuSettings);
 menu.setEventListeners();
 
 
+//Работа с попапом регистрации
+function openPopupLogin(evt) {
+  evt.preventDefault();
+  popupLogin.classList.add("popup_opened");
+}
 
+menuSettings.personalAccountButton.addEventListener("click", openPopupLogin);
+
+function closePopup(evt) {
+  if (
+    evt.target.classList.contains("popup__btn-close") &&
+    evt.target.closest(".popup").classList.contains("popup_opened")
+  ) {
+    evt.target.closest(".popup").classList.remove("popup_opened");
+  }
+}
+
+
+//Слушатели
+document.addEventListener("click", closePopup);
